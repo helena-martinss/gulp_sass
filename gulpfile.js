c789d0d3cfbfcc8d40713eabb16ce014ac2606c0
helena <helena257424@gmail.com>
2025-01-30 18:18:55 -0300

    Commit inicial

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+function funcaoPadrao(callback) {
+    setTimeout(function() {
+        console.log("Executando via Gulp");
+        callback();
+    }, 2000);
+}
+
+function dizOi(callback) {
+    setTimeout(function() {
+        console.log("Olá Gulp");
+        dizTchau();
+        callback();
+    }, 1000);
+}
+
+function dizTchau() {
+    console.log("Tchau Gulp");
+}
+
+exports.default = gulp.parallel(funcaoPadrao, dizOi);
+exports.dizOi = dizOi;
+exports.sass = compilaSass
